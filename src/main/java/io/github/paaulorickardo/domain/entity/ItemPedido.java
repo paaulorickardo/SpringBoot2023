9bc7c9da29fa4477c21f75c651ef4e6a0ee3bbf7
Paulo Ricardo <paaulorickardo@hotmail.com>
2023-03-28 10:59:20 -0300

    Salvando e recuperando clientes

--- src/main/java/io/github/paaulorickardo/domain/entity/ItemPedido.java
+++ src/main/java/io/github/paaulorickardo/domain/entity/ItemPedido.java
@@ -2,8 +2,8 @@ package io.github.paaulorickardo.domain.entity;
 
 public class ItemPedido {
     private Integer id;
-    private Pedido;
-    private Produto;
+    private Pedido pedido;
+    private Produto produto;
     private Integer quantidade;
 
     public Integer getId() {
@@ -12,6 +12,22 @@ public class ItemPedido {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
     }
 
     public Integer getQuantidade() {
