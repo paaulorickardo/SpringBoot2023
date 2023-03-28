08fd91886b2ee221737f61e574822220ee46a387
Paulo Ricardo <paaulorickardo@hotmail.com>
2023-03-28 10:08:03 -0300

    Projeto Spring 2023

---
+++ src/main/java/io/github/paaulorickardo/VendasApplication.java
@@ -0,0 +1,12 @@
+package io.github.paaulorickardo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+
+@SpringBootApplication
+public class VendasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+}
