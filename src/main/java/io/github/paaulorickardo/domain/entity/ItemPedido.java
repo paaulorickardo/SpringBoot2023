1327ca54ca12ea953ca8de7c4f48deadf8ad53bc
Paulo Ricardo <paaulorickardo@hotmail.com>
2023-03-28 10:16:36 -0300

    18.Criando as classes de modelo

---
+++ src/main/java/io/github/paaulorickardo/domain/entity/ItemPedido.java
@@ -0,0 +1,24 @@
+package io.github.paaulorickardo.domain.entity;
+
+public class ItemPedido {
+    private Integer id;
+    private Pedido;
+    private Produto;
+    private Integer quantidade;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+}
