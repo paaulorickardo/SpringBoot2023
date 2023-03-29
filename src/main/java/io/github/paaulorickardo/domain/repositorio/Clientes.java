76a68e8c38fbd5a32fefdc811ce2040f75af88f6
Paulo Ricardo <paaulorickardo@hotmail.com>
2023-03-29 09:49:01 -0300

    Persistindo entidades com Entity Manager

--- src/main/java/io/github/paaulorickardo/domain/repositorio/Clientes.java
+++ src/main/java/io/github/paaulorickardo/domain/repositorio/Clientes.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -19,10 +21,13 @@ public class Clientes {
     private static String DELETE = "delete from cliente where id = ? ";
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    public JdbcTemplate jdbcTemplate;
+    @Autowired
+    private EntityManager entityManager;
 
+    @Transactional
     public Cliente salvar(Cliente cliente){
-        jdbcTemplate.update( INSERT, new Object[]{cliente.getNome()} );
+        entityManager.persist(cliente);
         return cliente;
     }
 
