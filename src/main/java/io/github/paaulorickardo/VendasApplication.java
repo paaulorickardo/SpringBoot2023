0150984290e2f254791c5092e7ffc6a108b16b35
Paulo Ricardo <paaulorickardo@hotmail.com>
2023-03-29 09:23:29 -0300

    Concluindo cadastro de Clientes

--- src/main/java/io/github/paaulorickardo/VendasApplication.java
+++ src/main/java/io/github/paaulorickardo/VendasApplication.java
@@ -16,13 +16,39 @@ public class VendasApplication {
     @Bean
     public CommandLineRunner init(@Autowired Clientes clientes){
         return args -> {
+            System.out.println("Salvando clientes");
             clientes.salvar(new Cliente("Paulaoo"));
             clientes.salvar(new Cliente("Ricardao"));
 
             List<Cliente> todosClientes = clientes.obterTodos();
             todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando clientes");
+            todosClientes.forEach(c ->{
+                c.setNome(c.getNome() + "atualizado.");
+                clientes.atualizar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Buscando clientes");
+            clientes.buscarPorNome("Ric").forEach(System.out::println);
+
+//            System.out.println("deletando clientes");
+//            clientes.obterTodos().forEach(c -> {
+//                clientes.deletar(c);
+//            });
+
+            todosClientes = clientes.obterTodos();
+            if(todosClientes.isEmpty()){
+                System.out.println("Nenhum cliente encontrado.");
+            }else{
+                todosClientes.forEach(System.out::println);
+            }
         };
     }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
